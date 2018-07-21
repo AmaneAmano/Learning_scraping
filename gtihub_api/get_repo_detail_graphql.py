e038c0b0de69a7daed1e99f75e54172cbd1497dc
crimnut <agoota.90380@gmail.com>
2018-07-21 11:51:45 +0900

    fix comments rename filename

--- gtihub_api/get_repo_detail_graphql.py
+++ gtihub_api/get_repo_detail_graphql.py
@@ -1,4 +1,7 @@
-import emoji
+"""
+Get detail data of github repository using GitHub GraphQL API v4
+More information about Github GraphQL API v4 is here https://developer.github.com/v4/
+"""
 import os
 from dotenv import load_dotenv
 import requests
@@ -10,16 +13,16 @@ GITHUB_API_KEY = os.environ.get('GITHUB_API_KEY')
 ENDPOINT = 'https://api.github.com/graphql'
 
 
-def execute_query(api_key, query, endpoint='https://api.github.com/graphql'):
+def execute_query(api_key, query_string, endpoint='https://api.github.com/graphql'):
     """
     Execute query to GitHub GraphQL API.
-    :param api_key: git hub api key. you can use personal access token.
-    :param query:  the graphl query consists of a multi-line string.
-    :param endpoint: github graphql endpoint. default setting.
+    :param api_key: GitHub api key. You can use personal access token.
+    :param query_string:  the GitHub GraphQL query consists of a multi-line string.
+    :param endpoint: GitHub GraphAL endpoint. default setting.
     :return: json type response
     """
     headers = {'Authorization': f'token {api_key}'}
-    response = requests.post(endpoint, json={'query': query}, headers=headers)
+    response = requests.post(endpoint, json={'query': query_string}, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
@@ -34,21 +37,21 @@ def create_query_to_repo(owner_name, repo_name):
     :return: query
     """
     query = f"""
-    {{
+    query {{
       repository(owner: {owner_name}, name: {repo_name}){{
         name,
         nameWithOwner,
         description,
         createdAt,
         forkCount,
-        homepageUrl,
+        homepageUrl
         }}
     }}
     """
     return query
 
 
-query = create_query_to_repo(owner_name="vuejs", repo_name="vue")
+query = create_query_to_repo(owner_name="crimnut02", repo_name="aframe-mmd")
 
 json_data = execute_query(GITHUB_API_KEY, query, ENDPOINT)
 
