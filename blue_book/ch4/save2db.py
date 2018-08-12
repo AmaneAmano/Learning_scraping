7082fb32492a0abffad160c17e4b78d5da6308d9
crimnut <agoota.90380@gmail.com>
2018-08-12 23:44:55 +0900

    add

--- blue_book/ch4/save2db.py
+++ blue_book/ch4/save2db.py
@@ -1,1 +1,16 @@
-i
+import MySQLdb
+
+connection = MySQLdb.connect(
+    user='scrapingman',
+    passwd='@h]Pm/&.#5?NW,<L',
+    host='localhost',
+    db='scrapingdata',
+    charset='utf8'
+)
+
+cursor = connection.cursor()
+r = cursor.execute("INSERT INTO books VALUES (%s, %s)",
+                   ('Pythonによるクローラー＆スクレイピング入門 設計・開発から収集データの解析まで ', 'http://amzn.asia/cbfU3py'))
+print(r)
+connection.commit()
+connection.close()
